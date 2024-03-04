04393a15732a87eeb19871297b96c73d0a249909
Yang QiMing <yangqiming@gigacloudtech.com>
2024-03-04 19:52:07 +0800

    chore: commit postcss plugin bench

---
+++ bench/fixtures/tailwindcss/tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  darkMode: 'class',
+  content: ['../../source/*.js'],
+  warn: false
+}
---
+++ bench/meta.mjs
@@ -0,0 +1,23 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
+import { getPackageInfo } from 'local-pkg'
+import fs from 'fs-extra'
+
+export const dir = dirname(fileURLToPath(import.meta.url))
+
+export const targets = ['none', 'windicss', 'tailwindcss', 'unocss']
+
+const pkgs = ['vite', 'unocss', 'windicss', 'tailwindcss']
+
+export async function getVersions() {
+  const versions = Object.fromEntries(
+    await Promise.all(
+      pkgs.map(async (i) => [
+        i,
+        (await getPackageInfo(i))?.packageJson?.version
+      ])
+    )
+  )
+  versions.unocss = (await fs.readJSON(resolve(dir, '../package.json'))).version
+  return versions
+}
